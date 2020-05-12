5e23d127db0ca181feb7dcdfc6544b7e26fad010
sashagrona <sasha_grona@bigmir.net>
2020-05-13 00:04:40 +0300

    completed Dijkstra

--- src/com/company/dijkstra/DijkstraShortestPath.java
+++ src/com/company/dijkstra/DijkstraShortestPath.java
@@ -18,24 +18,36 @@ public class DijkstraShortestPath<T> {
         distances = new HashMap<>();
     }
 
-    public int dijkstra(T start) {
-        visited.add(start);
-        distances.put(start, 0);
+    public int getShortestPath(T from, T to) {
+        dijkstra(from);
+        return (distances.get(to) != null) ? distances.get(to) : -1;
+    }
+
+    public void dijkstra(T start) {
         PriorityQueue<Node> queue = new PriorityQueue<Node>(graph.getSize(), comparator);
         queue.add(new Node(start, 0));
+        distances.put(start, 0);
         while (!queue.isEmpty()) {
             Node<T> node = queue.poll();
+            visited.add(node.getValue());
             List<Edge<T>> edges = graph.getEdges(node.getValue());
             for (Edge<T> edge : edges) {
                 T to = edge.getTo();
-                int newDist = distances.get(edge.getFrom()) + edge.getCost();
-                if (distances.get(to) != null && (newDist < distances.get(to))) {
-                    
+                if (visited.contains(to)) {
+                    continue;
                 }
-
-
+                int newDist = distances.get(edge.getFrom()) + edge.getCost();
+                if (distances.get(to) != null) {
+                    if (newDist < distances.get(to)) {
+                        queue.remove(new Node(to, distances.get(to)));
+                        queue.add(new Node(to, newDist));
+                        distances.put(to, newDist);
+                    }
+                } else {
+                    distances.put(to, newDist);
+                    queue.add(new Node(to, newDist));
+                }
             }
         }
-        return -1;
     }
 }
