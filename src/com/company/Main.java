2fee736f4e590ea17c94fa158f3f716d596fdff1
sashagrona <sasha_grona@bigmir.net>
2020-05-23 21:34:32 +0300

    added Bellman Ford algorithm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,27 @@
 package com.company;
 
+import com.company.bellman.BellmanFord;
 import com.company.dijkstra.DijkstraShortestPath;
 import com.company.graph.edge.Graph;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         Graph<Integer> graph = new Graph<>();
-        graph.addEdge(0,1,5);
-        graph.addEdge(0,2,1);
-        graph.addEdge(1,3,3);
-        graph.addEdge(1,2,2);
-        graph.addEdge(1,4,20);
-        graph.addEdge(2,1,3);
-        graph.addEdge(2,4,12);
-        graph.addEdge(3,2,3);
-        graph.addEdge(3,4,2);
-        graph.addEdge(3,5,6);
-        graph.addEdge(4,5,1);
-        graph.addEdge(5,5,0);
-
-
+        graph.addEdge( 0, 1, 1);
+        graph.addEdge(1, 2, 1);
+        graph.addEdge(2, 4, 1);
+        graph.addEdge(4, 3, -3);
+        graph.addEdge(3, 2, 1);
+        graph.addEdge(1, 5, 4);
+        graph.addEdge(1, 6, 4);
+        graph.addEdge(5, 6, 5);
+        graph.addEdge(6, 7, 4);
+        graph.addEdge(5, 7, 3);
         System.out.println(graph.toString());
-        DijkstraShortestPath<Integer> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
-        System.out.println(dijkstraShortestPath.getIntPath(1,4));
+        BellmanFord bellmanFord = new BellmanFord(graph, 0, 9);
+        System.out.println(Arrays.toString(bellmanFord.getDistances()));
     }
 }
---
+++ src/com/company/bellman/BellmanFord.java
@@ -0,0 +1,41 @@
+package com.company.bellman;
+
+import com.company.graph.edge.Edge;
+import com.company.graph.edge.Graph;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BellmanFord {
+    private Graph<Integer> graph;
+    private int start;
+    private double [] distances;
+
+    public BellmanFord(Graph<Integer> graph, int start, int vertices) {
+        this.graph = graph;
+        this.start = start;
+        this.distances = new double[vertices];
+    }
+
+    public double[] getDistances(){
+        Arrays.fill(distances, Double.POSITIVE_INFINITY);
+        distances[start] = 0;
+        commonOperations(false);
+        //to find parts of negative cycle
+        commonOperations(true);
+        return distances;
+    }
+
+    private void commonOperations(boolean isInfiniteSearch){
+        for (int i=0;i<distances.length-1;i++){
+            for (int j = 0;j<graph.getSize();j++){
+                List<Edge<Integer>> edges = graph.getEdges(j);
+                for (Edge edge : edges){
+                    if(distances[(int) edge.getFrom()] + edge.getCost() < distances[(int) edge.getTo()]){
+                        distances[(int) edge.getTo()] = (!isInfiniteSearch) ? distances[(int)edge.getFrom()] + edge.getCost() : Double.NEGATIVE_INFINITY;
+                    }
+                }
+            }
+        }
+    }
+}
