3c73f42be47d2de3ebd4943ed0e6f4aaf0e30e86
sashagrona <sasha_grona@bigmir.net>
2020-06-07 21:34:40 +0300

    added combination and main algorithm for tsp

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,22 @@
 package com.company;
 
-import com.company.dfs.TarjanSolver;
-import com.company.graph.edge.Graph;
+import com.company.dynamicprog.TSPSolver;
+import com.company.graph.matrix.MatrixGraph;
 
 public class Main {
 
     public static void main(String[] args) {
-        Graph<Integer> graph = new Graph();
-        graph.addEdge(0, 1, 0);
-        graph.addEdge(0, 3, 0);
-        graph.addEdge(1, 2, 0);
-        graph.addEdge(1, 4, 0);
-        graph.addEdge(2, 0, 0);
-        graph.addEdge(2, 6, 0);
-        graph.addEdge(4, 5, 0);
-        graph.addEdge(4, 6, 0);
-        graph.addEdge(5, 6, 0);
-        graph.addEdge(5, 7, 0);
-        graph.addEdge(5, 8, 0);
-        graph.addEdge(5, 9, 0);
-        graph.addEdge(6, 4, 0);
-        graph.addEdge(7, 9, 0);
-        graph.addEdge(8, 9, 0);
-        graph.addEdge(9, 8, 0);
-        graph.addEdge(3, 3, 0);
+        MatrixGraph graph = new MatrixGraph(6);
+        graph.addEdge(0, 3, 8);
+        graph.addEdge(3, 2, 6);
+        graph.addEdge(2, 4, 4);
+        graph.addEdge(4, 1, 2);
+        graph.addEdge(1, 5, 12);
+        graph.addEdge(5, 1, 10);
+
         System.out.println(graph.toString());
-        TarjanSolver tarjanSolver = new TarjanSolver(graph);
-        tarjanSolver.findStronglyConnComps();
-        System.out.println(tarjanSolver);
+        TSPSolver tspSolver = new TSPSolver(0, graph);
+        System.out.println(tspSolver);
+        tspSolver.getCombinations(3);
     }
 }
--- src/com/company/dynamicprog/TSPSolver.java
+++ src/com/company/dynamicprog/TSPSolver.java
@@ -1,27 +1,90 @@
 package com.company.dynamicprog;
 
+import com.company.graph.matrix.MatrixGraph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //used to solve np-complete Travelling Salesman Problem (shortest tour path between all nodes)
 public class TSPSolver {
     private int size;
     private int start;
-    private double[][] distances;
+    private MatrixGraph graph;
     private double[][] memotable;
 
-    public TSPSolver(int start, double[][] distances) {
+    public TSPSolver(int start, MatrixGraph graph) {
         this.start = start;
-        this.distances = distances;
-        this.size = distances.length;
+        this.graph = graph;
+        this.size = graph.getSize();
         this.memotable = new double[size][1 << size];
-        if (size > 32) throw new IllegalArgumentException("The complexity of algorithm is O(n^2 * 2^n). Input is too large");
+        if (size > 32)
+            throw new IllegalArgumentException("The complexity of algorithm is O(n^2 * 2^n). Input is too large");
         init();
     }
 
     private void init() {
-        for (int i = 0; i< size;i++) {
+        for (int i = 0; i < size; i++) {
             if (i != start) {
-                memotable[i][1 << i | 1 << start] = distances[start][i];
+                memotable[i][1 << i | 1 << start] = (graph.getDistance(start, i) != Double.POSITIVE_INFINITY) ? graph.getDistance(start, i) : 0;
             }
         }
     }
-    
+
+    public void tsp() {
+        for (int i = 3; i <= size; i++) {
+            for (int set : getCombinations(i)) {
+                if (notInSubset(start, set)) continue;
+                for (int nextEl = 0; nextEl < size; nextEl++) {
+                    if (nextEl == start || notInSubset(nextEl, set)) continue;
+                    int setExclNext = set ^ (1 << nextEl);
+                    double minDistance = Double.POSITIVE_INFINITY;
+                    for (int end = 0; end < size; end++) {
+                        if (end == start || end == nextEl || notInSubset(end, set)) continue;
+                        double newDistance = memotable[end][setExclNext] + graph.getDistance(end, nextEl);
+                        if (newDistance < minDistance) {
+                            minDistance = newDistance;
+                        }
+                    }
+                    memotable[nextEl][set] = minDistance;
+                }
+            }
+        }
+    }
+
+    private boolean notInSubset(int el, int subset) {
+        return ((1 << el) & subset) == 0;
+    }
+
+    public List<Integer> getCombinations(int fillSize) {
+        List<Integer> combinations = new ArrayList<>();
+        setCombinations(0, 0, fillSize, combinations);
+        System.out.println(combinations);
+        return combinations;
+    }
+
+    private void setCombinations(int pointer, int set, int fillSize, List<Integer> combinations) {
+        if (fillSize == 0) {
+            combinations.add(set);
+        } else {
+            for (int i = pointer; i < size; i++) {
+                //adding new bit
+                set |= (1 << i);
+                setCombinations(i + 1, set, fillSize - 1, combinations);
+                //combination is set. exclude current bit and backtrack
+                set ^= (1 << i);
+            }
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        tsp();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            sb.append(Arrays.toString(memotable[i])).append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
 }
